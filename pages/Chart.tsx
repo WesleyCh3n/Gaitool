ce6208caad6bac0d8dfa1989c778f4df627d9684
WesleyCh3n <wesley.ch3n.0530@gmail.com>
2022-01-11 15:33:11 +0800

    🍁 separate backend origin

--- pages/Chart.tsx
+++ pages/Chart.tsx
@@ -76,7 +76,7 @@ function DrawChart(): ReactElement | null {
     const formData = new FormData();
     formData.append("file", f); // NOTE: append("key", value)
 
-    return fetch("/api/upload", { method: "POST", body: formData })
+    return fetch("http://localhost:3001/api/upload", { method: "POST", body: formData })
       .then((res) => res.json())
       .then((jsonRslt) => {
         Promise.all(
@@ -114,21 +114,23 @@ function DrawChart(): ReactElement | null {
     updators.lnav = createGaitNav(refs.lnav)
 
     // DUBUG:
-    Promise.all(csvFiles.map((file) => d3.csv(file))).then(
-      ([csvResult, csvGaitCycle, csvLtCycle, csvRtCycle, csvDbCycle]) => {
-        setDataS(parseResult(csvResult, dataS));
-        updateApp(dataS[selPos][selOpt], {
-          gait: parseCycle(csvGaitCycle),
-          lt: parseCycle(csvLtCycle),
-          rt: parseCycle(csvRtCycle),
-          db: parseCycle(csvDbCycle),
-        });
-        setSelDisable(false);
-      }
-    );
+    // Promise.all(csvFiles.map((file) => d3.csv(file))).then(
+      // ([csvResult, csvGaitCycle, csvLtCycle, csvRtCycle, csvDbCycle]) => {
+        // setDataS(parseResult(csvResult, dataS));
+        // updateApp(dataS[selPos][selOpt], {
+          // gait: parseCycle(csvGaitCycle),
+          // lt: parseCycle(csvLtCycle),
+          // rt: parseCycle(csvRtCycle),
+          // db: parseCycle(csvDbCycle),
+        // });
+        // setSelDisable(false);
+      // }
+    // );
   }, []);
 
   const updateLogic = (d: IData[], c: ICycleList) => {
+    // preprocess data -> boxplot
+    // filter cycle need to on top scope
     updators.line(d, c.gait);
     updators.bmax(d, c.gait);
     updators.bmin(d, c.gait);
