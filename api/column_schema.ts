ffe1e1c5583df11c271e65239bbd9c892d42906a
WesleyCh3n <wesley.ch3n.0530@gmail.com>
2022-04-28 17:48:08 +0800

    🔧 fix: use new column name

---
+++ api/column_schema.ts
@@ -0,0 +1,34 @@
+export const col_schema  = {
+  "L": {
+    "Accel X": { data: [], csvX: "time", csvY: "L Accel Sensor X (mG)" },
+    "Accel Y": { data: [], csvX: "time", csvY: "L Accel Sensor Y (mG)" },
+    "Accel Z": { data: [], csvX: "time", csvY: "L Accel Sensor Z (mG)" },
+    "Gyro X": { data: [], csvX: "time", csvY: "Noraxon MyoMotion-Segments-L-Gyroscope-x (deg/s)" },
+    "Gyro Y": { data: [], csvX: "time", csvY: "Noraxon MyoMotion-Segments-L-Gyroscope-y (deg/s)" },
+    "Gyro Z": { data: [], csvX: "time", csvY: "Noraxon MyoMotion-Segments-L-Gyroscope-z (deg/s)" },
+  },
+  "T": {
+    "Accel X": { data: [], csvX: "time", csvY: "T Accel Sensor X (mG)" },
+    "Accel Y": { data: [], csvX: "time", csvY: "T Accel Sensor Y (mG)" },
+    "Accel Z": { data: [], csvX: "time", csvY: "T Accel Sensor Z (mG)" },
+    "Gyro X": { data: [], csvX: "time", csvY: "Noraxon MyoMotion-Segments-T-Gyroscope-x (deg/s)" },
+    "Gyro Y": { data: [], csvX: "time", csvY: "Noraxon MyoMotion-Segments-T-Gyroscope-y (deg/s)" },
+    "Gyro Z": { data: [], csvX: "time", csvY: "Noraxon MyoMotion-Segments-T-Gyroscope-z (deg/s)" },
+  },
+  "Scapular LT": {
+    "Accel X": { data: [], csvX: "time", csvY: "Scapular LT Accel Sensor X (mG)" },
+    "Accel Y": { data: [], csvX: "time", csvY: "Scapular LT Accel Sensor Y (mG)" },
+    "Accel Z": { data: [], csvX: "time", csvY: "Scapular LT Accel Sensor Z (mG)" },
+    "Gyro X": { data: [], csvX: "time", csvY: "Noraxon MyoMotion-Segments-Scapular LT-Gyroscope-x (deg/s)" },
+    "Gyro Y": { data: [], csvX: "time", csvY: "Noraxon MyoMotion-Segments-Scapular LT-Gyroscope-y (deg/s)" },
+    "Gyro Z": { data: [], csvX: "time", csvY: "Noraxon MyoMotion-Segments-Scapular LT-Gyroscope-z (deg/s)" },
+  },
+  "Scapular RT": {
+    "Accel X": { data: [], csvX: "time", csvY: "Scapular RT Accel Sensor X (mG)" },
+    "Accel Y": { data: [], csvX: "time", csvY: "Scapular RT Accel Sensor Y (mG)" },
+    "Accel Z": { data: [], csvX: "time", csvY: "Scapular RT Accel Sensor Z (mG)" },
+    "Gyro X": { data: [], csvX: "time", csvY: "Noraxon MyoMotion-Segments-Scapular RT-Gyroscope-x (deg/s)" },
+    "Gyro Y": { data: [], csvX: "time", csvY: "Noraxon MyoMotion-Segments-Scapular RT-Gyroscope-y (deg/s)" },
+    "Gyro Z": { data: [], csvX: "time", csvY: "Noraxon MyoMotion-Segments-Scapular RT-Gyroscope-z (deg/s)" },
+  }
+}
